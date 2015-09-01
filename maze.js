bdfbbffc132ea7caab2d03ef4858cdd5fc885a72
mdang <dang.its.matthew@gmail.com>
2015-09-02 03:40:07 +1000

    hopefully wont crash my computer

--- maze.js
+++ maze.js
@@ -2,6 +2,9 @@ var HEIGHT = 80;
 var WIDTH = 80;
 var BLOCK_WIDTH = 10;
 
+var UNVISITED = 0;
+var VISITED = 1;
+
 var render = function(maze, canvas) {
 	for (var i = maze.length - 1; i >= 0; i--) {
 		for (var j = maze[i].length - 1; j >= 0; j--) {
@@ -10,9 +13,6 @@ var render = function(maze, canvas) {
 		};
 	};
 };
-
-var UNVISITED = 0;
-var VISITED = 1;
 
 var createMaze = function() {
 	var maze = new Array(WIDTH);
@@ -43,20 +43,30 @@ $(document).ready(function() {
 		y: 0
 	};
 
-	while (next.length > 0) {
+	var stack = [];
+	stack.push(cur);
+
+	var iterate = function() {
 		maze[cur.x][cur.y] = VISITED;
 
-		var next = [];
+		var neighbours = [];
 
-		if (cur.x > 0) next.push({ x: cur.x - 1, y: cur.y });
-		if (cur.x < WIDTH) next.push({ x: cur.x + 1, y: cur.y });
-		if (cur.y > 0) next.push({ x: cur.x, y: cur.y - 1 });
-		if (cur.y < HEIGHT) next.push({ x: cur.x, y: cur.y + 1 });
+		if (cur.x > 0 && maze[cur.x - 1][cur.y] === UNVISITED) neighbours.push({ x: cur.x - 1, y: cur.y });
+		if (cur.x < WIDTH - 1 && maze[cur.x + 1][cur.y] === UNVISITED) neighbours.push({ x: cur.x + 1, y: cur.y });
+		if (cur.y > 0 && maze[cur.x][cur.y - 1] === UNVISITED) neighbours.push({ x: cur.x, y: cur.y - 1 });
+		if (cur.y < HEIGHT - 1 && maze[cur.x][cur.y + 1] === UNVISITED) neighbours.push({ x: cur.x, y: cur.y + 1 });
 
-		cur = next[Math.random() * next.length];
-	}
+		while (neighbours.length > 0) {
+			stack.push(neighbours.splice(Math.random() * neighbours.length, 1)[0]);
+		}
 
+		cur = stack.pop();
 
-	render(maze, c);
+		render(maze, c);
+
+		if (stack.length > 0) setTimeout(iterate, 100);
+	};
+	
+	setTimeout(iterate, 100);
 });
 
