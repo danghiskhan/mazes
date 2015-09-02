42642074cbcc33285c5411cb2415cf7b7fe9819d
mdang <dang.its.matthew@gmail.com>
2015-09-02 16:09:31 +1000

    fixed bugs

--- maze.js
+++ maze.js
@@ -1,22 +1,20 @@
-var HEIGHT = 20;
-var WIDTH = 20;
-var W = 24;
+var HEIGHT = 30;
+var WIDTH = 30;
+var W = 50;
 
-var ITERATION_MS = 10;
+var ITERATION_MS = 1;
 
 var render = function(maze, canvas) {
+	canvas.fillStyle = "#000000";
+	canvas.fillRect(0, 0, (HEIGHT + 1 / 2) * W, (WIDTH + 1 / 2) * W);
+
 	for (var i = maze.length - 1; i >= 0; i--) {
 		for (var j = maze[i].length - 1; j >= 0; j--) {
-			canvas.fillStyle = "#000000";
-			canvas.fillRect(0, 0, i * W, j * W);
-
 			canvas.fillStyle = "#00FF00";
-			if (maze[i][j].visited) canvas.fillRect(i * W + W / 3, j * W + W / 3, W / 3, W / 3);
-			canvas.fillStyle = "#0000FF";
-			if (!maze[i][j].top) canvas.fillRect(i * W + W / 3, j * W, W / 3, W / 3);
-			if (!maze[i][j].bottom) canvas.fillRect(i * W + W / 3, j * W + 2 * W / 3, W / 3, W / 3);
-			if (!maze[i][j].right) canvas.fillRect(i * W + 2 * W / 3, j * W + W / 3, W / 3, W / 3);
-			if (!maze[i][j].left) canvas.fillRect(i * W, j * W + W / 3, W / 3, W / 3);
+			if (maze[i][j].visited) canvas.fillRect((i + 1 / 2) * W, (j + 1 / 2) * W, W / 2, W / 2);
+			canvas.fillStyle = "#00FFFF";
+			if (!maze[i][j].bottom) canvas.fillRect((i + 1 / 2) * W, (j + 1 / 2) * W + W / 2, W / 2, W / 2);
+			if (!maze[i][j].right) canvas.fillRect((i + 1 / 2) * W + W / 2, (j + 1 / 2) * W, W / 2, W / 2);
 		};
 	};
 };
@@ -31,10 +29,8 @@ var create2dArray = function(width, height) {
 		for (var j = a[i].length - 1; j >= 0; j--) {
 			a[i][j] = {
 				visited: false,
-				top: true,
 				right: true,
-				bottom: true,
-				left: true
+				bottom: true
 			};
 		};
 	};
@@ -43,7 +39,7 @@ var create2dArray = function(width, height) {
 }
 
 $(document).ready(function() {
-	var canvas = $("<canvas/>", { id: "maze" }).prop({ height: HEIGHT * W, width: WIDTH * W });
+	var canvas = $("<canvas/>", { id: "maze" }).prop({ height: (HEIGHT + 2) * W, width: (WIDTH + 2) * W });
 
 	$("body").append(canvas);
 
@@ -64,12 +60,18 @@ $(document).ready(function() {
 
 		var neighbours = [];
 
-		if (cur.x > 0 && !maze[cur.x - 1][cur.y].visited) neighbours.push({ x: cur.x - 1, y: cur.y });
-		if (cur.x < WIDTH - 1 && !maze[cur.x + 1][cur.y].visited) neighbours.push({ x: cur.x + 1, y: cur.y });
-		if (cur.y > 0 && !maze[cur.x][cur.y - 1].visited) neighbours.push({ x: cur.x, y: cur.y - 1 });
-		if (cur.y < HEIGHT - 1 && !maze[cur.x][cur.y + 1].visited) neighbours.push({ x: cur.x, y: cur.y + 1 });
-
-		var deadEnd = neighbours.length === 0;
+		if (cur.x > 0 && !maze[cur.x - 1][cur.y].visited) {
+			neighbours.push({ x: cur.x - 1, y: cur.y, dx: -1, dy: 0 });	
+		} 
+		if (cur.x < WIDTH - 1 && !maze[cur.x + 1][cur.y].visited) {
+			neighbours.push({ x: cur.x + 1, y: cur.y, dx: 1, dy: 0 });	
+		} 
+		if (cur.y > 0 && !maze[cur.x][cur.y - 1].visited) {
+			neighbours.push({ x: cur.x, y: cur.y - 1, dx: 0, dy: -1 });	
+		} 
+		if (cur.y < HEIGHT - 1 && !maze[cur.x][cur.y + 1].visited) {
+			neighbours.push({ x: cur.x, y: cur.y + 1, dx: 0, dy: 1 });	
+		} 
 
 		while (neighbours.length > 0) {
 			stack.push(neighbours.splice(Math.random() * neighbours.length, 1)[0]);
@@ -77,22 +79,18 @@ $(document).ready(function() {
 
 		var newCur = stack.pop();
 
-		var delta = {
-			x: newCur.x - cur.x, 
-			y: newCur.y - cur.y
-		};
-
-		if (!deadEnd) {
-			if (delta.x === 1) maze[cur.x][cur.y].right = false;
-			if (delta.x === -1) maze[cur.x][cur.y].left = false;
-			if (delta.y === 1) maze[cur.x][cur.y].top = false;
-			if (delta.y === -1) maze[cur.x][cur.y].bottom = false;
+		while (maze[newCur.x][newCur.y].visited) {
+			newCur = stack.pop();
 		}
 
+		if (newCur.dx === 1 && newCur.x > 0) maze[newCur.x - 1][newCur.y].right = false;
+		if (newCur.dy === 1 && newCur.y > 0) maze[newCur.x][newCur.y - 1].bottom = false;
+		if (newCur.dx === -1) maze[newCur.x][newCur.y].right = false;
+		if (newCur.dy === -1) maze[newCur.x][newCur.y].bottom = false;
+
 		cur = newCur;
-
+	
 		render(maze, c);
-
 		if (stack.length > 0) setTimeout(iterate, ITERATION_MS);
 	};
 	
