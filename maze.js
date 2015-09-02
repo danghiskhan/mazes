31ffc9e516aebbd2960d6cf52a3da4f2062b5c9b
mdang <dang.its.matthew@gmail.com>
2015-09-02 18:07:34 +1000

    added more bias options

--- maze.js
+++ maze.js
@@ -1,6 +1,6 @@
 var HEIGHT = 80;
 var WIDTH = 80;
-var W = 8;
+var W = 12;
 
 var INTERVAL_MS = 1;
 var STEPS_AT_ONCE = 50;
@@ -45,76 +45,153 @@ var create2dArray = function(width, height) {
 	return a;
 }
 
-$(document).ready(function() {
-	var canvas = $("<canvas/>", { id: "maze" }).prop({ height: (HEIGHT + 2) * W, width: (WIDTH + 2) * W });
+// Randomly chooses path
+var randomPath = function(paths) {
+	var newPaths = [];
 
-	$("body").append(canvas);
+	while (paths.length > 0) {
+		newPaths.push(paths.splice(Math.random() * paths.length, 1)[0]);
+	}
+
+	return newPaths;
+};
+
+// Biased towards horizontal paths
+var horizontalBias = function(paths) {
+	return paths.sort(function(a, b) {
+		return Math.abs(a.dx) + Math.abs(b.dx) - Math.random() * 8;
+	});
+};
+
+// Biased towards diagonal paths
+var diagonalBias = function(paths) {
+	return paths.sort(function(a, b) {
+		return 1 + Math.abs(a.x) + Math.abs(a.y) - Math.abs(b.x) - Math.abs(b.y) - Math.random() * 4;
+	});
+};
+
+// Center bias
+var centerBias = function(paths) {
+	return paths.sort(function(a, b) {
+		var distToCenter = function(pos) {
+			return Math.abs(pos.x - WIDTH / 2) + Math.abs(pos.y - HEIGHT / 2);
+		};
+
+		return 1 + distToCenter(a) - distToCenter(b) - Math.random() * 2;
+	});
+};
+
+// Outer bias
+var outerBias = function(paths) {
+	return paths.sort(function(a, b) {
+		var distToCenter = function(pos) {
+			return Math.abs(pos.x - WIDTH / 2) + Math.abs(pos.y - HEIGHT / 2);
+		};
+
+		return 1 + distToCenter(b) - distToCenter(a) - Math.random() * 2;
+	});
+};
+
+
+var biases = [
+	{ name: 'Outer Bias', fun: outerBias },
+	{ name: 'Center Bias', fun: centerBias },
+	{ name: 'Random', fun: randomPath }, 
+	{ name: 'Horizontal Bias', fun: horizontalBias }, 
+	{ name: 'Diagonal Bias', fun: diagonalBias }
+];
+
+var setupDropdown = function() {
+	var dropdown = 	$('#biases');
+
+	$.each(biases, function(index, value) {
+		dropdown.append($("<option />").val(index).text(value.name));
+	});
+};
+
+$(document).ready(function() {
+	setupDropdown();
+
+	var canvas = $("<canvas/>", { id: "maze" }).prop({ height: (HEIGHT + 1 / 2) * W, width: (WIDTH + 1 / 2) * W });
+
+	$(".container").append(canvas);
 
 	var c = canvas[0].getContext("2d");
 
-	var maze = create2dArray(HEIGHT, WIDTH);
+	var runMaze = function() {
+		var maze = create2dArray(HEIGHT, WIDTH);
 
-	var cur = {
-		x: 0,
-		y: 0
-	};
+		var cur = {
+			x: 0,
+			y: 0
+		};
 
-	var stack = [];
-	stack.push(cur);
+		var stack = [];
+		stack.push(cur);
 
-	var i = 0;
+		var i = 0;
 
-	var iterate = function() {
-		if (stack.length === 0) return;
+		var iterate = function() {
+			if (stack.length === 0) return;
 
-		maze[cur.x][cur.y].visited = true;
+			maze[cur.x][cur.y].visited = true;
 
-		var neighbours = [];
+			var neighbours = [];
 
-		// Add possible paths to stack
-		if (cur.x > 0 && !maze[cur.x - 1][cur.y].visited) {
-			neighbours.push({ x: cur.x - 1, y: cur.y, dx: -1, dy: 0 });	
-		} 
-		if (cur.x < WIDTH - 1 && !maze[cur.x + 1][cur.y].visited) {
-			neighbours.push({ x: cur.x + 1, y: cur.y, dx: 1, dy: 0 });	
-		} 
-		if (cur.y > 0 && !maze[cur.x][cur.y - 1].visited) {
-			neighbours.push({ x: cur.x, y: cur.y - 1, dx: 0, dy: -1 });	
-		} 
-		if (cur.y < HEIGHT - 1 && !maze[cur.x][cur.y + 1].visited) {
-			neighbours.push({ x: cur.x, y: cur.y + 1, dx: 0, dy: 1 });	
-		} 
+			// Add possible paths to stack
+			if (cur.x > 0 && !maze[cur.x - 1][cur.y].visited) {
+				neighbours.push({ x: cur.x - 1, y: cur.y, dx: -1, dy: 0 });	
+			} 
+			if (cur.x < WIDTH - 1 && !maze[cur.x + 1][cur.y].visited) {
+				neighbours.push({ x: cur.x + 1, y: cur.y, dx: 1, dy: 0 });	
+			} 
+			if (cur.y > 0 && !maze[cur.x][cur.y - 1].visited) {
+				neighbours.push({ x: cur.x, y: cur.y - 1, dx: 0, dy: -1 });	
+			} 
+			if (cur.y < HEIGHT - 1 && !maze[cur.x][cur.y + 1].visited) {
+				neighbours.push({ x: cur.x, y: cur.y + 1, dx: 0, dy: 1 });	
+			}
 
-		while (neighbours.length > 0) {
-			stack.push(neighbours.splice(Math.random() * neighbours.length, 1)[0]);
-		}
+			// Sort via path criteria
+			neighbours = biases[parseInt($('#biases').val())].fun(neighbours);
 
-		// Get the next path ignoring paths to already visited nodes
-		cur = stack.pop();
-		while (stack.length > 0 && maze[cur.x][cur.y].visited) {
+			while (neighbours.length > 0) {
+				stack.push(neighbours.pop());
+			}
+
+			// Get the next path ignoring paths to already visited nodes
 			cur = stack.pop();
-		}
+			while (stack.length > 0 && maze[cur.x][cur.y].visited) {
+				cur = stack.pop();
+			}
 
-		// Mark the wall to the new path as destroyed
-		if (cur.dx === 1 && cur.x > 0) maze[cur.x - 1][cur.y].right = false;
-		if (cur.dy === 1 && cur.y > 0) maze[cur.x][cur.y - 1].bottom = false;
-		if (cur.dx === -1) maze[cur.x][cur.y].right = false;
-		if (cur.dy === -1) maze[cur.x][cur.y].bottom = false;
+			// Mark the wall to the new path as destroyed
+			if (cur.dx === 1 && cur.x > 0) maze[cur.x - 1][cur.y].right = false;
+			if (cur.dy === 1 && cur.y > 0) maze[cur.x][cur.y - 1].bottom = false;
+			if (cur.dx === -1) maze[cur.x][cur.y].right = false;
+			if (cur.dy === -1) maze[cur.x][cur.y].bottom = false;
 
-		// Render the maze in its current state
-		if (i++ % STEPS_AT_ONCE === 0 || stack.length === 0) render(maze, c);
+			// Render the maze in its current state
+			if (i++ % STEPS_AT_ONCE === 0 || stack.length === 0) render(maze, c);
+		};
+
+
+		var faster_iterator = function() {
+			for (var i = 0; i < STEPS_AT_ONCE; i++) {
+				iterate();	
+			}
+
+			// If incomplete, iterate again
+			if (stack.length > 0) setTimeout(faster_iterator, INTERVAL_MS);
+		};
+		
+		setTimeout(faster_iterator, INTERVAL_MS);
 	};
 
+	runMaze();
 
-	var faster_iterator = function() {
-		for (var i = 0; i < STEPS_AT_ONCE; i++) {
-			iterate();	
-		}
-
-		// If incomplete, iterate again
-		if (stack.length > 0) setTimeout(faster_iterator, INTERVAL_MS);
-	};
-	
-	setTimeout(faster_iterator, INTERVAL_MS);
+	$('#biases').change(function(e) {
+		runMaze();
+	});
 });
 
